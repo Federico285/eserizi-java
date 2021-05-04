8529861e76690c073ca84290c0b6e360c2c3660a
Federico285 <70782902+Federico285@users.noreply.github.com>
2021-05-04 15:39:52 +0200

    Add files via upload

---
+++ Volo.java
@@ -0,0 +1,16 @@
+package classi;
+
+/**
+ * la classe volo è identificata da:\n
+ * id numerico;\n
+ * stringa di destinazione (sigla);\n
+ * stringa di partenza (sigla);\n
+ * numero massimo di passeggeri;\n
+ * 
+ * ha due costruttori:\n
+ * 1. 
+ */
+
+public class Volo {
+
+}
